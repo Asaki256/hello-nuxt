b0b0aa685f66bff76613ad48e6c4195cb1524911
Masaki <77131849+Asaki256@users.noreply.github.com>
2024-06-14 23:58:43 +0900

    P.123 useAsyncData()のpickオプションとtransformオプション

--- pages/WeatherInfo/[id].vue
+++ pages/WeatherInfo/[id].vue
@@ -10,7 +10,8 @@ const selectedCity = computed((): City => {
   return cityList.value.get(idNo) as City;
 });
 
-const weatherDescription = ref("");
+// const weatherDescription = ref("");
+
 // const weatherInfoUrl = "https://api.openweathermap.org/data/2.5/weather";
 // const params: {
 //   lang: string;
@@ -28,26 +29,38 @@ const weatherDescription = ref("");
 // const weather = weatherArray[0];
 // weatherDescription.value = weather.description;
 
-const asyncData = await useAsyncData(`/WeatherInfo/${route.params.id}`, (): Promise<any> => {
-  const weatherInfoUrl = "https://api.openweathermap.org/data/2.5/weather";
-  const params: {
-    lang: string;
-    q: string;
-    appid: string;
-  } = {
-    lang: "ja",
-    q: selectedCity.value.q,
-    appid: "6abde7b29586ba11ec133c6cf457c334",
-  };
-  const queryParams = new URLSearchParams(params);
-  const urlFull = `${weatherInfoUrl}?${queryParams}`;
-  const response = $fetch(urlFull);
-  return response;
-});
-const data = asyncData.data;
-const weatherArray = data.value.weather;
-const weather = weatherArray[0];
-weatherDescription.value = weather.description;
+const asyncData = await useAsyncData(
+  `/WeatherInfo/${route.params.id}`,
+  (): Promise<any> => {
+    const weatherInfoUrl = "https://api.openweathermap.org/data/2.5/weather";
+    const params: {
+      lang: string;
+      q: string;
+      appid: string;
+    } = {
+      lang: "ja",
+      q: selectedCity.value.q,
+      appid: "6abde7b29586ba11ec133c6cf457c334",
+    };
+    const queryParams = new URLSearchParams(params);
+    const urlFull = `${weatherInfoUrl}?${queryParams}`;
+    const response = $fetch(urlFull);
+    return response;
+  },
+  {
+    // pick: ["weather"],
+    transform: (data: any): string => {
+      const weatherArray = data.weather;
+      const weather = weatherArray[0];
+      return weather.description;
+    },
+  }
+);
+// const data = asyncData.data;
+// const weatherArray = data.value.weather;
+// const weather = weatherArray[0];
+// weatherDescription.value = weather.description;
+const weatherDescription = asyncData.data;
 </script>
 
 <template>
