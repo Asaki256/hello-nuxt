5e1280deea8446598ef8945fe230bd6896830372
Masaki <77131849+Asaki256@users.noreply.github.com>
2024-06-25 22:26:59 +0900

    P.194 エラーの基礎, 子コンポ描画時のエラー処理

---
+++ pages/errorHandlerBasic.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const onResetButtonClick = (error: Ref) => {
+  error.value = null;
+};
+</script>
+
+<template>
+  <!-- NuxtErrorBoundaryで囲んだとこでエラーキャッチできるよ -->
+  <NuxtErrorBoundary>
+    <ErrorGeneratorBasic />
+    <p>テストコメント</p>
+    <!-- NuxtErrorBoundaryの子要素にtemplateを配置してv-slotでerrorを受け取ることで表示も可能！ -->
+    <template v-slot:error="errorProps">
+      <p>エラー発生した！</p>
+      <p>{{ errorProps.error }}</p>
+      <button @click="onResetButtonClick(errorProps.error)">エラーを解消</button>
+    </template>
+  </NuxtErrorBoundary>
+  <p>
+    <NuxtLink :to="{ name: 'index' }"> 戻る</NuxtLink>
+  </p>
+</template>
