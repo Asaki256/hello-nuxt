b4a91b9dd5a8d14d0bacf54fbe510639638224d6
Masaki <77131849+Asaki256@users.noreply.github.com>
2024-06-27 23:04:36 +0900

    P.202 カスタムエラー

--- components/ErrorGeneratorFatal.vue
+++ components/ErrorGeneratorFatal.vue
@@ -2,7 +2,9 @@
 const onThrowErrorClick = (): void => {
   throw createError({
     message: "致命的な擬似エラー発生！",
+    // statusCode: 404,
     fatal: true,
+    statusMessage: "ステータスメッセージ。果たしてこれはどこに配置されるのか。",
   });
 };
 </script>
