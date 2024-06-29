616f4d95257d761d864d112ce97376b14ae1e48f
Masaki <77131849+Asaki256@users.noreply.github.com>
2024-06-29 23:58:50 +0900

    P.208 try-catch エラー処理の雛形

--- server/routes/member-management/members.get.ts
+++ server/routes/member-management/members.get.ts
@@ -3,14 +3,22 @@ import type {Member, ReturnJSONMembers} from "@/interfaces";
 export default defineEventHandler(async(event):Promise<ReturnJSONMembers>=> {
   // 空の会員リストのMapオブジェクト生成
   let memberList = new Map<number, Member>();
-  // ローカルストレージから会員リストを取得
-  const storage = useStorage();
-  const memberListStorage = await storage.getItem("local:member-management_members");
-  // ローカルストレージから取得できた場合はMapオブジェクトに変換
-  if(memberListStorage != undefined){
-    // Nuxtではstorage.getItemで取得した値は文字列でなくJSON形式で返されるため、変換の手間が減っている
-    // anyで変換する前はStorageValueで返されるため、anyで強制的にMapに変換する
-    memberList = new Map<number, Member>(memberListStorage as any);
+  let resultVal = 0;
+
+  // throw createError("擬似エラー発生")
+  try {
+    // ローカルストレージから会員リストを取得
+    const storage = useStorage();
+    const memberListStorage = await storage.getItem("local:member-management_members");
+    // ローカルストレージから取得できた場合はMapオブジェクトに変換
+    if(memberListStorage != undefined){
+      // Nuxtではstorage.getItemで取得した値は文字列でなくJSON形式で返されるため、変換の手間が減っている
+      // anyで変換する前はStorageValueで返されるため、anyで強制的にMapに変換する
+      memberList = new Map<number, Member>(memberListStorage as any);
+    }
+      resultVal = 1;
+  } catch(err) {
+    console.log(err);
   }
   // Mapオブジェクトから値のみを取得
   const memberListValues = memberList.values();
@@ -18,7 +26,7 @@ export default defineEventHandler(async(event):Promise<ReturnJSONMembers>=> {
   const memberListArray = Array.from(memberListValues);
   // 取得した会員リストを返す
   return {
-    result: 1,
+    result: resultVal,
     data: memberListArray,
   }
 })
